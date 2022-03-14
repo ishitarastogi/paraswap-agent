21442b3c9a7f2f028cdb032364210bdf0d5e31e9
ishita rastogi <46647968+ishitarastogi@users.noreply.github.com>
2022-03-14 21:28:19 +0530

    Update agent.spec.ts

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -7,7 +7,7 @@ import {
 } from "forta-agent";
 import { createAddress, TestTransactionEvent } from "forta-agent-tools";
 import { provideHandleTransaction } from "./agent";
-import {utils,BigNumber } from "ethers";
+import { utils, BigNumber } from "ethers";
 import util from "./utils";
 
 const testAugustus: string = createAddress("0xdef1");
@@ -16,14 +16,17 @@ const USER_ADDR = createAddress("0xf1e4a");
 const iface: utils.Interface = new utils.Interface([
   util.SET_IMPLEMENTATION,
   util.SET_FEE_WALLET,
-  util.TRANSFER_TOKENS
+  util.TRANSFER_TOKENS,
+  util.REGISTER_PARTNER,
+  util.ADAPTER_INITIALIZED,
+  util.ROUTER_INITIALIZED,
 ]);
 
 const createTransaction = (functionName: string, params: any[]) => {
   return iface.encodeFunctionData(functionName, [...params]);
 };
 
-const createFinding = (Name:string,params: any) => {
+const createFinding = (Name: string, params: any) => {
   const metadata: any = {};
   for (const key in params) {
     metadata[key] = params[key];
@@ -67,7 +70,7 @@ describe("Large stake deposits", () => {
     const findings = await handler(txEvent);
     console.log("F1", findings);
 
-    const expectedFinding = createFinding("setImplementation",{
+    const expectedFinding = createFinding("setImplementation", {
       selector: testBytes.toLowerCase(),
       implementation: testImplementation,
     });
@@ -77,7 +80,7 @@ describe("Large stake deposits", () => {
   });
 
   it("should return a Finding from setFeeWallet function call", async () => {
-    const testAddress: string = createAddress("0xaBc");
+    const testAddress: string = createAddress("0xabc");
 
     const Transaction: string = createTransaction("setFeeWallet", [
       testAddress,
@@ -90,7 +93,7 @@ describe("Large stake deposits", () => {
 
     const findings = await handler(txEvent);
     console.log("f2", findings);
-    const expectedFinding = createFinding("setFeeWallet",{
+    const expectedFinding = createFinding("setFeeWallet", {
       _feeWallet: testAddress,
     });
 
@@ -99,13 +102,13 @@ describe("Large stake deposits", () => {
   });
 
   it("should return a Finding from transfer function call", async () => {
-    const testTokenAddress: string = createAddress("0xAA");
-const testDestinationAddress:string = createAddress("0xAA");
-const testAmount:BigNumber=BigNumber.from(100);
+    const testTokenAddress: string = createAddress("0xb1");
+    const testDestinationAddress: string = createAddress("0xb2");
+    const testAmount: BigNumber = BigNumber.from(100);
     const Transaction: string = createTransaction("transferTokens", [
       testTokenAddress,
       testDestinationAddress,
-      testAmount
+      testAmount,
     ]);
 
     const txEvent: TransactionEvent = new TestTransactionEvent()
@@ -115,14 +118,95 @@ const testAmount:BigNumber=BigNumber.from(100);
 
     const findings = await handler(txEvent);
     console.log("f2", findings);
-    const expectedFinding = createFinding("transferTokens",{
-      address: testTokenAddress,
-      destination:testDestinationAddress,
-      amount:testAmount
+    const expectedFinding = createFinding("transferTokens", {
+      token: testTokenAddress,
+      destination: testDestinationAddress,
+      amount: testAmount,
     });
 
     console.log("l2", expectedFinding);
     expect(findings).toStrictEqual([expectedFinding]);
   });
+  it("should return a Finding from register partner function call", async () => {
+    const testPartnerAddress: string = createAddress("0xb1");
+    const testPartnerShare: BigNumber = BigNumber.from(100);
+    const testNoPositiveSlippage: boolean = true;
+    const testPositiveSlippageToUser: boolean = false;
+    const testFeePercent: number = 1;
+    const testPartnerId: string = "partnerId";
+    const testData: string = "0xda12";
+    const Transaction: string = createTransaction("registerPartner", [
+      testPartnerAddress,
+      testPartnerShare,
+      testNoPositiveSlippage,
+      testPositiveSlippageToUser,
+      testFeePercent,
+      testPartnerId,
+      testData,
+    ]);
 
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setTo(testAugustus)
+      .setFrom(USER_ADDR)
+      .setData(Transaction);
+
+    const findings = await handler(txEvent);
+    console.log("f2", findings);
+    const expectedFinding = createFinding("registerPartner", {
+      partner: testPartnerAddress,
+      _partnerShare: testPartnerShare,
+      _noPositiveSlippage: testNoPositiveSlippage,
+      _positiveSlippageToUser: testPositiveSlippageToUser,
+      _feePercent: testFeePercent,
+      partnerId: testPartnerId,
+      _data: testData,
+    });
+
+    console.log("l2", expectedFinding);
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
+  it("should return findings from adapter initialized call", async () => {
+    const testAdapter: string = createAddress("0xd34d");
+
+    const { data, topics } = iface.encodeEventLog(
+      iface.getEvent("AdapterInitialized"),
+      [testAdapter]
+    );
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(USER_ADDR)
+      .addAnonymousEventLog(testAugustus, data, ...topics);
+
+    const findings = await handler(txEvent);
+    const expectedFinding = createFinding("AdapterInitialized", {
+      adapter: testAdapter,
+    });
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
+  it("should return findings from adapter initialized call", async () => {
+    const testRouter: string = createAddress("0xd34d");
+
+    const { data, topics } = iface.encodeEventLog(
+      iface.getEvent("RouterInitialized"),
+      [testRouter]
+    );
+
+    const txEvent: TransactionEvent = new TestTransactionEvent()
+      .setFrom(USER_ADDR)
+      .addAnonymousEventLog(testAugustus, data, ...topics);
+
+    const findings = await handler(txEvent);
+    const expectedFinding = createFinding("RouterInitialized", {
+      router: testRouter,
+    });
+    expect(findings).toStrictEqual([expectedFinding]);
+  });
+  it("should return no findings from incorrect address", async () => {
+
+  });
+  it("should return findings from incorrect event signature", async () => {
+
+  });
+  it("should return findings for multiple function calls for transferTokens", async ()=>{
+  })
 });
